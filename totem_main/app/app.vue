67868582685f8973e0201afeb43aa859e34974c3
jonas lara <jonassilvadelara@gmail.com>
2026-07-04 00:48:28 -0300

    chore: add tailwild

--- totem_main/app/app.vue
+++ totem_main/app/app.vue
@@ -1,6 +1,14 @@
+<script lang="ts" setup>
+  useSeoMeta({
+    title: 'Página principal',
+    description: 'esta é a página principal',
+  })
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen flex items-center justify-center bg-gray-900">
+    <h1 class="text-5xl font-bold text-blue-400">
+      Hello Colégios
+    </h1>
   </div>
 </template>
--- totem_vocacional/app/app.vue
+++ totem_vocacional/app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen flex items-center justify-center bg-gray-500">
+    <h1 class="text-5xl font-bold text-blue-400">
+      Hello Vocacional
+    </h1>
   </div>
 </template>
