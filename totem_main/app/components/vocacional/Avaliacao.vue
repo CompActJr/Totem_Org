82ffc7d9081962e1ac2c6752ec718e6f175f158d
jonas lara <jonassilvadelara@gmail.com>
2026-07-07 18:08:28 -0300

    fix: question e options

--- totem_main/app/components/vocacional/Avaliacao.vue
+++ totem_main/app/components/vocacional/Avaliacao.vue
@@ -15,14 +15,16 @@ const {
 const currentAnswer = computed(() => getCurrentAnswer());
 
 const currentQuestion = computed(() => {
-    return data.find(q => q.id === state.value.currentQuestion);
+    return data.find(q => q.id === state.value.currentQuestion + 1);
 });
+
+console.log(currentQuestion);
 
 function selectOption(option: Option) {
 
     saveAnswer({
         questionId: state.value.currentQuestion,
-        options: [option.id]
+        options: option
     });
 
     if (state.value.currentQuestion < data.length) {
@@ -38,18 +40,51 @@ function selectOption(option: Option) {
         Avaliação
     </h2>
 
-    <ClientOnly>
-        <p></p>
+    <ClientOnly v-if="currentQuestion">
+        <p class="text-center text-gray-600">
+            Pergunta {{ state.currentQuestion + 1 }} de {{ data.length }}
+        </p>
+        <article class="text-left text-gray-600 text-lg">
+            {{ currentQuestion.titulo }}
+        </article>
+
+        <div class="mt-8">
+            <div v-for="option in currentQuestion.opcoes" :key="option.id">
+                <input type="checkbox" />
+                <span>{{ option.text }}</span>
+            </div>
+        </div>
+
     </ClientOnly>
-  
 
-    <div class="flex justify-between">
-        <UButton color="neutral" variant="outline" @click="previousStep">
-            Voltar
-        </UButton>
 
-        <UButton @click="nextStep">
-            Finalizar
-        </UButton>
-    </div>
+    <footer class="flex items-center justify-between">
+        <button class=" transition-all duration-300 hover:scale-110 bg-orange-400 
+                p-2 w-16 rounded-full text-white
+                flex items-center justify-between font-bold text-4-lg"
+            @click="previousQuestion"
+            :disabled="state.currentQuestion + 1 <= 1"
+        >
+            <<
+        </button>
+
+        <button class=" transition-all duration-300 hover:scale-110 bg-orange-400 
+                p-2 w-46 rounded-full text-white
+                flex items-center justify-between font-bold text-4-lg"
+            @click="previousStep"
+        >
+            Voltar ao cadastro
+        </button>
+
+        <button 
+            class="transition-all duration-300 hover:scale-110 bg-orange-400 
+                p-2 w-16 rounded-full text-white
+                flex items-center justify-between font-bold text-4-lg"
+            :disabled="state.currentQuestion +1 >= data.length"
+            @click="nextQuestion"
+        >
+            >>
+        </button>
+
+    </footer>
 </template>
