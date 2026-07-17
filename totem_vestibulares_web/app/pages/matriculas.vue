efe1af1d70142faf2aa08e5084e00af377eec7be
Jean PC <jeanalemao2006@gmail.com>
2026-07-17 09:30:31 -0300

    Inserindo os components na pagina matriculas

--- totem_vestibulares_web/app/pages/matriculas.vue
+++ totem_vestibulares_web/app/pages/matriculas.vue
@@ -1,3 +1,14 @@
-<template>
+<script setup lang="ts">
+import GridMatricula from '~/components/matricula/GridMatricula.vue';
+import HeroMatricula from '~/components/matricula/HeroMatricula.vue';
+import IntroMatricula from '~/components/matricula/IntroMatricula.vue';
 
+</script>
+
+<template>
+    <HeroMatricula />
+
+    <IntroMatricula />
+
+    <GridMatricula />
 </template>
