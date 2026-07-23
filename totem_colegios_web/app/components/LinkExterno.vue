94aa937cba4ac8b6b9c0d48cc910ad940814bd94
jonas lara <jonassilvadelara@gmail.com>
2026-07-22 21:47:18 -0300

    feat: parceiros

---
+++ totem_colegios_web/app/components/LinkExterno.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+/**
+ * @author Jonas
+ * Componente para link externos das páginas
+ */
+    interface Props{
+        externalLink: string
+    }
+
+    const props = defineProps<Props>();
+</script>
+
+<template>
+    <NuxtLink :to="props.externalLink" external target="_blank" class="
+                group
+                mt-8
+                inline-flex
+                w-fit
+                items-center
+                gap-2
+                rounded-full
+                bg-white
+                px-8
+                py-3
+                font-bold
+                text-gray-700
+                shadow-xl
+                transition-all
+                duration-300
+                hover:-translate-y-1
+                hover:bg-orange-500
+                hover:text-white
+                hover:shadow-2xl
+                active:scale-95
+            ">
+        ACESSE
+
+        <span class="
+                    transition-transform
+                    duration-300
+                    group-hover:translate-x-1
+                ">
+            →
+        </span>
+    </NuxtLink>
+</template>
--- totem_colegios_web/app/pages/unidades/santa-maria/index.vue
+++ totem_colegios_web/app/pages/unidades/santa-maria/index.vue
@@ -7,6 +7,7 @@ import HeroUnidadeTitle from '~/components/unidade/HeroUnidadeTitle.vue';
 import SecaoAtividades from '~/components/unidade/SecaoAtividades.vue';
 import SecaoInfra from '~/components/unidade/SecaoInfra.vue';
 import SecaoNiveisEnsino from '~/components/unidade/SecaoNiveisEnsino.vue';
+import SecaoParceiros from '~/components/unidade/SecaoParceiros.vue';
 import SecaoProfessores from '~/components/unidade/SecaoProfessores.vue';
 import type { AtividadeType } from '~/types/atividades.types';
 
@@ -53,6 +54,8 @@ const professores = repository.getProfessores(unidade.id)
     <SecaoInfra :unidade="unidade"/>
 
     <SecaoAtividades :atividades="atividades"/>
+
+    <SecaoParceiros/>
 
     <SecaoProfessores :professores="professores"/>
 
